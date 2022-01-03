10b24282dc65d719c555b6a6c6e1a6ef38b96a54
gabriela <ticugabriela12@gmail.com>
2022-01-04 00:09:01 +0300

    feat: add buy button

--- js/script.js
+++ js/script.js
@@ -1,29 +1,33 @@
 var countDownDate = new Date("Jan 7, 2022 00:00:00").getTime();
 var x = setInterval(function() {
-  var now = new Date().getTime();
-  var distance = countDownDate - now;
-    
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  document.getElementById("timer").innerHTML = days + "д " + hours + "ч " + minutes + "м";
-  
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("timerText").innerHTML = "Покупай мой курс!";
-    document.getElementById("timer").innerHTML = "";
-  }
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    document.getElementById("timer").innerHTML = days + "д " + hours + "ч " + minutes + "м";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("timerText").innerHTML = "Привет! Меня зовут Никочка!";
+        document.getElementById("timer").innerHTML = "Покупай мой курс :)";
+        var buttons = document.getElementsByClassName("buyCourse");
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].style.display = "block";
+        }
+    }
 }, 1000);
 
 //timer block animation
 const observer = new IntersectionObserver(entries => {
     // перебор записей
     entries.forEach(entry => {
-      // если элемент появился
-      if (entry.isIntersecting) {
-        // добавить ему CSS-класс
-        entry.target.classList.add('appearLeft');
-      }
+        // если элемент появился
+        if (entry.isIntersecting) {
+            // добавить ему CSS-класс
+            entry.target.classList.add('appearLeft');
+        }
     });
-  });
-  observer.observe(document.querySelector('.timerBlock'));
+});
+observer.observe(document.querySelector('.timerBlock'));
