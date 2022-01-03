15df3d279b956229c3fe5077a4514a42b43063a0
qbic2002 <golovinmax28@gmail.com>
2022-01-03 16:32:01 +0300

    fix: delete seconds from timer

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ var x = setInterval(function() {
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
   document.getElementById("timer").innerHTML = days + "д " + hours + "ч " + minutes + "м";
   
   if (distance < 0) {
