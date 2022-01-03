4751738ecc8458df3c20b6975f362ea8c58bc982
qbic2002 <golovinmax28@gmail.com>
2022-01-03 15:37:46 +0300

    fix: change timer message

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById("timer").innerHTML = days + "Д " + hours + "Ч";
+  document.getElementById("timer").innerHTML = days + "д " + hours + "ч " + minutes + "м";
   
   if (distance < 0) {
     clearInterval(x);
